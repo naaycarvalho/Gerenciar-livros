46d92301b2855cc43304136e9817666ef73076c9
LUCAS LEOCADIO <lucasitalo@frwk.com.br>
2024-11-24 14:17:04 -0300

    conflito home

--- gerenciarlivros/src/pages/home.jsx
+++ gerenciarlivros/src/pages/home.jsx
@@ -1,22 +1,5 @@
-import { Container, Image } from "react-bootstrap";
-import livrosImg from "../assets/img/livros.jpg";
-import "./home.css";
-
-function Home() {
-    return (
-        <>
-            <h2 className="text-center">Bem-vindo ao Sistema!</h2>
-            <Container className="d-flex justify-content-center align-items-center my-4">
-                <Image 
-                    src={livrosImg} 
-                    alt="Livros" 
-                    fluid // Para a imagem ser responsiva
-                    rounded // Estiliza a imagem com bordas arredondadas
-                    className="livros-img"
-                />
-            </Container>
-        </>
-    )
+function Home(){
+    return(<h1>Bem-vindo ao Sistema</h1>)
 }
-    
+
 export default Home;
