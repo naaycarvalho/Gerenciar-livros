94a84a8a1da1b705b17b3c1c7c8c2fe782374b91
LUCAS LEOCADIO <lucasitalo@frwk.com.br>
2024-11-24 20:04:29 -0300

    mensagem para usuario

--- gerenciarlivros/src/pages/Usuarios.jsx
+++ gerenciarlivros/src/pages/Usuarios.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Stack from 'react-bootstrap/Stack';
+import Alert from 'react-bootstrap/Alert';
 import FormUser from '../Componentes/Usuarios/FormUser';
 import ListUser from '../Componentes/Usuarios/ListUser';
 import './usuarios.css';
@@ -9,24 +10,55 @@ const usuarioService = new UsuarioService();
 
 function Usuarios() {
   const [usuarioId, setUsuarioId] = useState(0);
+  const [alert, setAlert] = useState({
+    show: false,
+    variant:'success',
+    message: ''
+  });
 
   useEffect(() => {
-    console.log('usuarioId',usuarioId)
-  }, [usuarioId]);
+    if (alert.show) {
+      const timer = setTimeout(() => {
+        handleCloseAlert();
+      }, 1000); // Tempo do delay em milissegundos (10 segundos)
 
+      // Limpa o timer se o componente for desmontado ou se o alerta for fechado antes
+      return () => clearTimeout(timer);
+    }
+  }, [usuarioId, alert]);
+
+  // Mostrar alerta
+  const handleShowAlert = (type, text) => {
+    setAlert({
+      show: true,
+      variant: type,
+      message: text
+    });
+  }
+
+  // Fecha o alerta
+  const handleCloseAlert = () => {
+    setAlert({
+      show: false,
+      variant:'success',
+      message: ''
+    }); 
+  };
   // Função para adicionar um novo usuário
   const salvarUsuario = (usuario) => {
     if (usuarioId > 0) {
       usuarioService.atualizarUsuario(usuarioId, usuario).then((response) => {
-        console.log(response.data);
+        handleShowAlert('success', response.message);
       }).catch((erro) => {
-        console.error('Erro ao cadastrar o usuário:', erro);
+        console.error('Erro ao alterar o usuário:', erro);
+        handleShowAlert('danger', 'Erro ao alterar o usuário.');
       });
     } else {
       usuarioService.cadastrarUsuario(usuario).then((response) => {
-        console.log(response.data);
+        handleShowAlert('success', response.message);
       }).catch((erro) => {
         console.error('Erro ao cadastrar o usuário:', erro);
+        handleShowAlert('danger', 'Erro ao cadastrar o usuário.');
       });
     }
     cancelar();
@@ -43,9 +75,10 @@ function Usuarios() {
 
     if (confirmar) {
       usuarioService.deletarUsuario(id).then((response) => {
-        console.log(response.message);
+        handleShowAlert('success', 'Usuário excluido com sucesso.');
       }).catch((erro) => {
         console.error('Erro ao excluir o usuário:', erro);
+        handleShowAlert('danger', 'Erro ao excluir o usuário.');
       });
     }
   };
@@ -55,14 +88,19 @@ function Usuarios() {
   }
 
   return (
-    <Stack gap={2} className='usuarios'>
-      <div className="p-2">
-        <FormUser usuarioId={usuarioId} onSalvarUsuario={salvarUsuario} onCancelar={cancelar} />
-      </div>
-      <div className="p-2">
-        <ListUser onEditarUsuario={editarUsuario} onExcluirUsuario={excluirUsuario} />
-      </div>
-    </Stack>
+    <>
+      <Alert className='usuario-alert' show={alert.show} variant={alert.variant} onClose={handleCloseAlert} dismissible>
+        <p>{alert.message}</p>
+      </Alert>
+      <Stack gap={2} className='usuarios'>
+        <div className="p-2">
+          <FormUser usuarioId={usuarioId} onSalvarUsuario={salvarUsuario} onCancelar={cancelar} />
+        </div>
+        <div className="p-2">
+          <ListUser onEditarUsuario={editarUsuario} onExcluirUsuario={excluirUsuario} />
+        </div>
+      </Stack>
+    </>
   )
   }
   export default Usuarios;
